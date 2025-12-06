b77ff625cbd485a33c72faff038cc9f4a61a0588
laptech88 <laptech88@gmail.com>
2025-12-06 21:36:43 +0500

    Vivid premiumm

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>
+      <Link href={"/dashboard"}>
         Vivid
-      </Button>
+      </Link>
 
       {/* <ModeToggle/> */}
         
